d0ed9f1e43f3ebf4455bad8c5a67a0bc4c2aecbe
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-08-04 09:34:35 -0300

    teste deploy 4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = { customCssUrl: '/swagger-ui.css'}
+const swaggerOptions = { customCssUrl: './public/swagger-ui.css'}
 const routes = require('./src/routes');
 const authDocProducao = require('./src/middlewares/autoDoc');
 
