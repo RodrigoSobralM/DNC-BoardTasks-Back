0d51e4f475c73bfb510b94444cf9beea3b7234ac
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-08-04 13:31:52 -0300

    teste deploy 5

--- src/middlewares/autoDoc.js
+++ src/middlewares/autoDoc.js
@@ -1,34 +1,33 @@
-const authDocProducao = async (req, res, next) => {
+async function authDocProducao(req, res, next) {
     const { senhaDigitada } = req.body;
 
-    if(req.headers.host.includes("localhost") || req.originalUrl !== "/doc/" ) {
-        //usuario esta no localhost
-        return next()
+    if(req.headers.host.includes("localhost") || req.originalUrl !== "/doc/"){
+        // Usuario está no localhost 
+        return next();
     }
 
-    if(senhaDigitada === process.env.SWAGGER_SENHA_DOC) {
-        //usuario digitou a senha certa
-        return next()
+    if(senhaDigitada === process.env.SWAGGER_SENHA_DOC){
+        // Usuario digitou a senha certa
+        return next();
     }
 
-    if(senhaDigitada) {
-        //usuario digitou a senha errada
-        res.status(401).set('Content-Type','text/html')
+    if(senhaDigitada){
+        // Usuario digitou a senha errada
+        res.status(401).set('Content-Type', 'text/html');
         res.send(Buffer.from(`
             <form method="post">
                 <p style="color: red;">Senha Errada!</p>
-                <label for="senhaDigitada"> Senha da documentação:</label>
+                <label for="senhaDigitada">Senha da documentação:</label>
                 <input type="password" name="senhaDigitada" id="senhaDigitada" />
                 <button type="submit">Entrar</button>
             </form>
         `))
-    } else {
-        //usuario ainda n digitou a senha e esta em modo produção
-        res.status(200).set('Content-Type','text/html')
+    } else{
+        // Usuario ainda não digitou a senha e está em modo produção
+        res.status(200).set('Content-Type', 'text/html');
         res.send(Buffer.from(`
             <form method="post">
-                <p style="color: red;">Senha Errada!</p>
-                <label for="senhaDigitada"> Senha da documentação:</label>
+                <label for="senhaDigitada">Senha da documentação:</label>
                 <input type="password" name="senhaDigitada" id="senhaDigitada" />
                 <button type="submit">Entrar</button>
             </form>
@@ -36,4 +35,4 @@ const authDocProducao = async (req, res, next) => {
     }
 }
 
-module.exports = authDocProducao
+module.exports = authDocProducao;
